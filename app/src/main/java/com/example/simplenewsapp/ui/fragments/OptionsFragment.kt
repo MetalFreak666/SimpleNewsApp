4ff16687839893d7317b31949d1e6293e9e8b5b3
Dariusz Orasinski <daor@houseofcode.io>
2021-03-26 19:23:21 +0100

    Added more news source options

--- app/src/main/java/com/example/simplenewsapp/ui/fragments/OptionsFragment.kt
+++ app/src/main/java/com/example/simplenewsapp/ui/fragments/OptionsFragment.kt
@@ -43,6 +43,26 @@ class OptionsFragment : Fragment(R.layout.fragment_options) {
             save_options_fab.isEnabled = true
         }
 
+        options_gb_icon.setOnClickListener {
+            countryCode = "gb"
+            save_options_fab.isEnabled = true
+        }
+
+        options_bg_icon.setOnClickListener {
+            countryCode = "bg"
+            save_options_fab.isEnabled = true
+        }
+
+        options_ru_icon.setOnClickListener {
+            countryCode = "ru"
+            save_options_fab.isEnabled = true
+        }
+
+        options_cz_icon.setOnClickListener {
+            countryCode = "cz"
+            save_options_fab.isEnabled = true
+        }
+
         save_options_fab.setOnClickListener {
             viewModel.getNews(countryCode, 1)
 
