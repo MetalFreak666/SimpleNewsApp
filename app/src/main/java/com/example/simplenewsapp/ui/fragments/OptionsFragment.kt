ee096688efef2851551662a6e159f49ac119c520
Dariusz Orasinski <daor@houseofcode.io>
2021-03-26 16:12:26 +0100

    Added clickable options item to the navigation menu

---
+++ app/src/main/java/com/example/simplenewsapp/ui/fragments/OptionsFragment.kt
@@ -0,0 +1,10 @@
+package com.example.simplenewsapp.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.simplenewsapp.R
+
+/**
+ * Fragment used to select options such as news source
+ */
+class OptionsFragment : Fragment(R.layout.fragment_options) {
+}
